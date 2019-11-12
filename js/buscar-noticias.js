6336938d730104b02eedfe09246fadf0714ed99e
beatrizsilvazup <beatriz.silva@zup.com.br>
2019-11-12 19:07:25 -0300

    adiciona trabalhando com o dom e js

--- js/buscar-noticias.js
+++ js/buscar-noticias.js
@@ -1,38 +1,56 @@
 var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=912011b8918b488585b24f26546053cb';
+  'country=us&' +
+  'apiKey=912011b8918b488585b24f26546053cb';
 var req = new Request(url);
 fetch(req)
-    .then(function(response) {
-        response = response.json()
-       .then(function(data) {
-            var data = data.articles;
-            showCards(data) 
-        })
-    });
+  .then(function (response) {
+    response = response.json()
+      .then(function (data) {
+        var data = data.articles;
+        console.log(data);
+      })
+  });
 
-    window.addEventListener("load", showCards)
+function showCards(data){
+  for (var i = 0; i < data.length; i++) {
+    var card = document.querySelector('.card-ultnoticias');
+    var article = document.createElement('div');
+    var myImg = document.createElement('img');
+    var conteudo = document.createElement('div');
+    var myH2 = document.createElement('h2');
+    var mySpan = document.createElement('span');
+    var myP = document.createElement('p');
 
-    function showCards(data) { 
-        for (var i = 0; i < data.length; i++) {
-          var myArticle = document.createElement('div');
-          var myImg = document.createElement('img');
-          var myDiv2 = document.createElement('div');
-          var myH2 = document.createElement('h2');
-          var mySpan = document.createElement('span');
-          var myPara = document.createElement('p');
+    article.classList.add('card-ultnoticias');
+    myImg.classList.add('card-ultnoticias__img');
+    conteudo.classList.add('card-ultnoticias__body');
 
-          myImg.innerHTML = data[i]["urlToImage"];
-          myArticle.appendChild(myImg)
+    myImg.innerHTML = data[i]["urlToImage"];
+    myH2.innerHTML = data[i]["title"];
+    mySpan.innerHTML = data[i]["author"];
 
-          var sectionAtual = document.getElementById("card-ultnoticias");
-          document.body.insertBefore(myArticle, sectionAtual)
-        }
-      }
+    card.appendChild(article);
+    article.appendChild(myImg);
+    article.appendChild(conteudo);
+    conteudo.appendChild(myH2);
+    conteudo.appendChild(span);
+    conteudo.appendChild(myP);
 
-    // createElement
-    // queryselector
-    // getelementbyid
-    // classname
-    // addclasslist
-    // textContent
+    // var card = document.getElementById('card-ultnoticias');
+    // var myArticle = document.createElement('div');
+    // myArticle.classList.add("card-ultnoticias")
+    
+    // card.appendChild(myArticle)
+    // var myImg = document.createElement('img');
+    // myArticle.appendChild(myImg)
+    // myImg.innerHTML = data[i]["urlToImage"];
+    
+  }
+}
+
+// createElement
+// queryselector
+// getelementbyid
+// classname
+// addclasslist
+// textContent
