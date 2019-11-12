cec008d9289c939ade5ac5f9dcbb39329438c555
beatrizsilvazup <beatriz.silva@zup.com.br>
2019-11-11 22:44:52 -0300

    adiciona api percorrendo o dom

--- js/buscar-noticias.js
+++ js/buscar-noticias.js
@@ -4,7 +4,35 @@ var url = 'https://newsapi.org/v2/top-headlines?' +
 var req = new Request(url);
 fetch(req)
     .then(function(response) {
-        console.log(response.json().then(function(body){
-            console.log('body', body)
-        }));
-    })
+        response = response.json()
+       .then(function(data) {
+            var data = data.articles;
+            showCards(data) 
+        })
+    });
+
+    window.addEventListener("load", showCards)
+
+    function showCards(data) { 
+        for (var i = 0; i < data.length; i++) {
+          var myArticle = document.createElement('div');
+          var myImg = document.createElement('img');
+          var myDiv2 = document.createElement('div');
+          var myH2 = document.createElement('h2');
+          var mySpan = document.createElement('span');
+          var myPara = document.createElement('p');
+
+          myImg.innerHTML = data[i]["urlToImage"];
+          myArticle.appendChild(myImg)
+
+          var sectionAtual = document.getElementById("card-ultnoticias");
+          document.body.insertBefore(myArticle, sectionAtual)
+        }
+      }
+
+    // createElement
+    // queryselector
+    // getelementbyid
+    // classname
+    // addclasslist
+    // textContent
